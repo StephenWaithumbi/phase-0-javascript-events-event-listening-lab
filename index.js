c9b423c224618a5bd0953d2dfb45a5e4df8d0428
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-10-06 00:30:07 +0300

    this was a miracle

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', call)
+}
+addingEventListener();
+
+function call() {
+    alert('I was clicked')
 }
